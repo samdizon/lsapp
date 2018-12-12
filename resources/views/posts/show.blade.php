2adfe7b900e215546055b64fc769459ee9882935
samdizon <dizon.sophia@gmail.com>
2018-12-13 06:15:23 +0800

    Post show with edit and delete button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Hellow</h1>
+    <div class="row">
+        <div class="col-8">
+            <h1>{{$post->title}}</h1>  
+        </div>
+        <div class="col-4 text-right">
+            <a class="btn btn-outline-secondary" href="/posts/{{$post->id}}/edit">Edit</a>
+            <a class="btn btn-outline-danger" href="/posts/{{$post->id}}">Delete</a>
+        </div>
+    </div>
+    <hr>
+    <div>
+        <p>{{$post->body}}</p>
+    </div>
+    <small>Written on {{$post->created_at}}</small>
 @endsection
--- storage/framework/views/b4b78b7a31531d4e8ddf98112cc09d54ba62ed99.php
+++ storage/framework/views/b4b78b7a31531d4e8ddf98112cc09d54ba62ed99.php
@@ -1,4 +1,17 @@
 <?php $__env->startSection('content'); ?>
-    <h1>Hellow</h1>
+    <div class="row">
+        <div class="col-8">
+            <h1><?php echo e($post->title); ?></h1>  
+        </div>
+        <div class="col-4 text-right">
+            <a class="btn btn-outline-secondary" href="/posts/<?php echo e($post->id); ?>/edit">Edit</a>
+            <a class="btn btn-outline-danger" href="/posts/<?php echo e($post->id); ?>">Delete</a>
+        </div>
+    </div>
+    <hr>
+    <div>
+        <p><?php echo e($post->body); ?></p>
+    </div>
+    <small>Written on <?php echo e($post->created_at); ?></small>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
